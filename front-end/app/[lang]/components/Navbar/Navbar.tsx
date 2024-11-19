aee8610c983f12fb208d5a7f766b2e5b6b3e9f99
Asun Gomez Castro <agcastro7@gmail.com>
2024-11-19 15:18:07 +0100

    Replace image with icon

--- front-end/app/[lang]/components/Navbar/Navbar.tsx
+++ front-end/app/[lang]/components/Navbar/Navbar.tsx
@@ -8,7 +8,11 @@ import {
   MenuItem,
   MenuItems,
 } from "@headlessui/react";
-import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline";
+import {
+  Bars3Icon,
+  XMarkIcon,
+  UserCircleIcon,
+} from "@heroicons/react/24/outline";
 import Image from "next/image";
 import logo from "./logo.svg";
 import { Container } from "../Container/Container";
@@ -79,14 +83,10 @@ export const Navbar: FC = async () => {
             <LanguageSelector />
             <Menu as="div" className="relative ml-3">
               <div>
-                <MenuButton className="relative flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+                <MenuButton className="relative rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                   <span className="absolute -inset-1.5" />
                   <span className="sr-only">Open user menu</span>
-                  <img
-                    alt=""
-                    src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                    className="size-8 rounded-full"
-                  />
+                  <UserCircleIcon className="size-6" />
                 </MenuButton>
               </div>
               <MenuItems
